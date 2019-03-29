2cf25e864f7e63bc0ac2393ada9d2b12e67920ec
Val Voronov <i.lightspark@gmail.com>
2019-03-29 22:47:34 +0700

    Moved a lot of stuff to tables

--- oUF_FloatingCombatFeedback.lua
+++ oUF_FloatingCombatFeedback.lua
@@ -26,31 +26,95 @@ local SCHOOL_MASK_SHADOW = _G.SCHOOL_MASK_SHADOW or 0x20
 local SCHOOL_MASK_ARCANE = _G.SCHOOL_MASK_ARCANE or 0x40
 
 local colors = {
-	ABSORB		= {r = 1.00, g = 1.00, b = 1.00},
-	BLOCK		= {r = 1.00, g = 1.00, b = 1.00},
-	DEFLECT		= {r = 1.00, g = 1.00, b = 1.00},
-	DODGE		= {r = 1.00, g = 1.00, b = 1.00},
-	ENERGIZE	= {r = 0.41, g = 0.80, b = 0.94},
-	EVADE		= {r = 1.00, g = 1.00, b = 1.00},
-	HEAL		= {r = 0.10, g = 0.80, b = 0.10},
-	IMMUNE		= {r = 1.00, g = 1.00, b = 1.00},
-	INTERRUPT	= {r = 1.00, g = 1.00, b = 1.00},
-	MISS		= {r = 1.00, g = 1.00, b = 1.00},
-	PARRY		= {r = 1.00, g = 1.00, b = 1.00},
-	REFLECT		= {r = 1.00, g = 1.00, b = 1.00},
-	RESIST		= {r = 1.00, g = 1.00, b = 1.00},
-	WOUND		= {r = 0.70, g = 0.10, b = 0.10},
+	["ABSORB"   ] = {r = 1.00, g = 1.00, b = 1.00},
+	["BLOCK"    ] = {r = 1.00, g = 1.00, b = 1.00},
+	["DEFLECT"  ] = {r = 1.00, g = 1.00, b = 1.00},
+	["DODGE"    ] = {r = 1.00, g = 1.00, b = 1.00},
+	["ENERGIZE" ] = {r = 0.41, g = 0.80, b = 0.94},
+	["EVADE"    ] = {r = 1.00, g = 1.00, b = 1.00},
+	["HEAL"     ] = {r = 0.10, g = 0.80, b = 0.10},
+	["IMMUNE"   ] = {r = 1.00, g = 1.00, b = 1.00},
+	["INTERRUPT"] = {r = 1.00, g = 1.00, b = 1.00},
+	["MISS"     ] = {r = 1.00, g = 1.00, b = 1.00},
+	["PARRY"    ] = {r = 1.00, g = 1.00, b = 1.00},
+	["REFLECT"  ] = {r = 1.00, g = 1.00, b = 1.00},
+	["RESIST"   ] = {r = 1.00, g = 1.00, b = 1.00},
+	["WOUND"    ] = {r = 0.70, g = 0.10, b = 0.10},
 }
 
 local schoolColors = {
-	[SCHOOL_MASK_NONE]		= {r = 1.00, g = 1.00, b = 1.00},
-	[SCHOOL_MASK_PHYSICAL]	= {r = 0.70, g = 0.10, b = 0.10},
-	[SCHOOL_MASK_HOLY]		= {r = 1.00, g = 0.90, b = 0.50},
-	[SCHOOL_MASK_FIRE]		= {r = 1.00, g = 0.50, b = 0.00},
-	[SCHOOL_MASK_NATURE]	= {r = 0.30, g = 1.00, b = 0.30},
-	[SCHOOL_MASK_FROST]		= {r = 0.50, g = 1.00, b = 1.00},
-	[SCHOOL_MASK_SHADOW]	= {r = 0.50, g = 0.50, b = 1.00},
-	[SCHOOL_MASK_ARCANE]	= {r = 1.00, g = 0.50, b = 1.00},
+	[SCHOOL_MASK_ARCANE  ] = {r = 1.00, g = 0.50, b = 1.00},
+	[SCHOOL_MASK_FIRE    ] = {r = 1.00, g = 0.50, b = 0.00},
+	[SCHOOL_MASK_FROST   ] = {r = 0.50, g = 1.00, b = 1.00},
+	[SCHOOL_MASK_HOLY    ] = {r = 1.00, g = 0.90, b = 0.50},
+	[SCHOOL_MASK_NATURE  ] = {r = 0.30, g = 1.00, b = 0.30},
+	[SCHOOL_MASK_NONE    ] = {r = 1.00, g = 1.00, b = 1.00},
+	[SCHOOL_MASK_PHYSICAL] = {r = 0.70, g = 0.10, b = 0.10},
+	[SCHOOL_MASK_SHADOW  ] = {r = 0.50, g = 0.50, b = 1.00},
+}
+
+local animations = {
+	["fountain"] = function(self)
+		return self.x + self.xDirection * 65 * (1 - m_cos(m_pi / 2 * self.elapsed / self.scrollTime)),
+			self.y + self.yDirection * 65 * m_sin(m_pi / 2 * self.elapsed / self.scrollTime)
+	end,
+	["vertical"] = function(self)
+		return self.x,
+			self.y + self.yDirection * 65 * self.elapsed / self.scrollTime
+	end,
+	["horizontal"] = function(self)
+		return self.x  + self.xDirection * 65 * self.elapsed / self.scrollTime,
+			self.y
+	end,
+	["diagonal"] = function(self)
+		return self.x  + self.xDirection * 65 * self.elapsed / self.scrollTime,
+			self.y + self.yDirection * 65 * self.elapsed / self.scrollTime
+	end,
+}
+
+local animationsByEvent = {
+	["ABSORB"   ] = "fountain",
+	["BLOCK"    ] = "fountain",
+	["DEFLECT"  ] = "fountain",
+	["DODGE"    ] = "fountain",
+	["ENERGIZE" ] = "fountain",
+	["EVADE"    ] = "fountain",
+	["HEAL"     ] = "fountain",
+	["IMMUNE"   ] = "fountain",
+	["INTERRUPT"] = "fountain",
+	["MISS"     ] = "fountain",
+	["PARRY"    ] = "fountain",
+	["REFLECT"  ] = "fountain",
+	["RESIST"   ] = "fountain",
+	["WOUND"    ] = "fountain",
+}
+
+local animationsByFlag = {
+	-- [" "       ] = "fountain",
+	-- ["CRITICAL"] = "fountain",
+	-- ["CRUSHING"] = "fountain",
+	-- ["GLANCING"] = "fountain",
+}
+
+local xOffsetsByAnimation = {
+	["diagonal"  ] = 24,
+	["fountain"  ] = 24,
+	["horizontal"] = 8,
+	["vertical"  ] = 8,
+}
+
+local yOffsetsByAnimation = {
+	["diagonal"  ] = 8,
+	["fountain"  ] = 8,
+	["horizontal"] = 8,
+	["vertical"  ] = 8,
+}
+
+local multipliersByFlag = {
+	[" "       ] = 1,
+	["CRITICAL"] = 1.25,
+	["CRUSHING"] = 1.25,
+	["GLANCING"] = 0.75,
 }
 
 local function removeString(self, i, string)
@@ -72,16 +136,6 @@ local function getAvailableString(self)
 	return removeString(self, 1, self.FeedbackToAnimate[1])
 end
 
-local function fountainScroll(self)
-	return self.x + self.xDirection * 65 * (1 - m_cos(m_pi / 2 * self.elapsed / self.scrollTime)),
-		self.y + self.yDirection * 65 * m_sin(m_pi / 2 * self.elapsed / self.scrollTime)
-end
-
-local function standardScroll(self)
-	return self.x,
-		self.y + self.yDirection * 65 * self.elapsed / self.scrollTime
-end
-
 local function onUpdate(self, elapsed)
 	for index, string in pairs(self.FeedbackToAnimate) do
 		if string.elapsed >= string.scrollTime then
@@ -89,7 +143,7 @@ local function onUpdate(self, elapsed)
 		else
 			string.elapsed = string.elapsed + elapsed
 
-			string:SetPoint("CENTER", self, "CENTER", self.Scroll(string))
+			string:SetPoint("CENTER", self, "CENTER", string:GetXY())
 
 			if (string.elapsed >= self.fadeout) then
 				string:SetAlpha(m_max(1 - (string.elapsed - self.fadeout) / (self.scrollTime - self.fadeout), 0))
@@ -110,11 +164,9 @@ end
 
 local function Update(self, _, unit, event, flag, amount, school)
 	if self.unit ~= unit then return end
-
 	local element = self.FloatingCombatFeedback
-	local multiplier = 1
-	local text, color
 
+	local text, color
 	if event == "WOUND" and not element.ignoreDamage then
 		if amount ~= 0	then
 			if element.abbreviateNumbers then
@@ -125,12 +177,6 @@ local function Update(self, _, unit, event, flag, amount, school)
 
 			color = element.schoolColors and element.schoolColors[school] or schoolColors[school]
 				or element.colors and element.colors[event] or colors[event]
-
-			if flag == "CRITICAL" or flag == "CRUSHING" then
-				multiplier = 1.25
-			elseif flag == "GLANCING" then
-				multiplier = 0.75
-			end
 		elseif flag and flag ~= "CRITICAL" and flag ~= "CRUSHING" and flag ~= "GLANCING" and not element.ignoreMisc then
 			text = _G[flag]
 			color = element.colors and element.colors[flag] or colors[flag]
@@ -143,10 +189,6 @@ local function Update(self, _, unit, event, flag, amount, school)
 		end
 
 		color = element.colors and element.colors[event] or colors[event]
-
-		if flag == "CRITICAL" then
-			multiplier = 1.25
-		end
 	elseif event == "HEAL" and not element.ignoreHeal then
 		if element.abbreviateNumbers then
 			text = "+"..AbbreviateNumbers(amount)
@@ -155,27 +197,27 @@ local function Update(self, _, unit, event, flag, amount, school)
 		end
 
 		color = element.colors and element.colors[event] or colors[event]
-
-		if flag == "CRITICAL" then
-			multiplier = 1.25
-		end
 	elseif not element.ignoreMisc then
 		text = _G[event]
 		color = element.colors and element.colors[event] or colors[event]
 	end
 
 	if text then
+		local animation = element.animationsByFlag and element.animationsByFlag[flag] or animationsByFlag[flag]
+			or element.animationsByEvent and element.animationsByEvent[event] or animationsByEvent[event]
+			or "fountain"
 		local string = getAvailableString(element)
 
 		string.elapsed = 0
+		string.GetXY = animations[animation]
 		string.scrollTime = element.scrollTime
 		string.xDirection = element.xDirection
 		string.yDirection = element.yDirection
-		string.x = element.xOffset * string.xDirection
-		string.y = element.yOffset * string.yDirection
+		string.x = (element.xOffsetsByAnimation and element.xOffsetsByAnimation[animation] or xOffsetsByAnimation[animation]) * string.xDirection
+		string.y = (element.yOffsetsByAnimation and element.yOffsetsByAnimation[animation] or yOffsetsByAnimation[animation]) * string.yDirection
 
 		string:SetText(text)
-		string:SetTextHeight(element.fontHeight * multiplier)
+		string:SetTextHeight(element.fontHeight * (multipliersByFlag[flag] or multipliersByFlag[" "]))
 		string:SetTextColor(color.r, color.g, color.b)
 		string:SetPoint("CENTER", element, "CENTER", string.x, string.y)
 		string:SetAlpha(1)
@@ -201,22 +243,13 @@ local function Enable(self)
 
 	element.__owner = self
 	element.ForceUpdate = ForceUpdate
+	element.FeedbackToAnimate = {}
 
 	element.scrollTime = element.scrollTime or 1.5
 	element.fadeout = element.scrollTime / 3
+	element.fontHeight = element.fontHeight or 18
 	element.xDirection = 1
 	element.yDirection = element.yDirection or 1
-	element.yOffset = element.yOffset or 8
-	element.fontHeight = element.fontHeight or 18
-	element.FeedbackToAnimate = {}
-
-	if element.mode == "Fountain" then
-		element.xOffset = element.xOffset or 6
-		element.Scroll = fountainScroll
-	else
-		element.xOffset = element.xOffset or 30
-		element.Scroll = standardScroll
-	end
 
 	for i = 1, #element do
 		element[i]:Hide()
